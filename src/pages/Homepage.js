c576c71befaee1e127ac78ec64aa3f2b50aa8cf4
Allen Feng <haha30932@gmail.com>
2023-08-08 15:03:00 +0800

    add more pic function

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,8 +4,10 @@ import Search from "../components/Search";
 import Picture from "../components/Picture";
 
 const HomePage = () => {
-  let [data, setData] = useState(null);
-  let [input, setInput] = useState([]);
+  let [data, setData] = useState([]);
+  let [input, setInput] = useState("");
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState();
 
   const auth = "BINLWIYb62j0NNqH5pvoWJSYbhwfbq6JXC6yDzkqQmsXCoKYLPU0KqDr";
   const initURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
@@ -14,6 +16,24 @@ const HomePage = () => {
   const search = async (url) => {
     let result = await axios.get(url, { headers: { Authorization: auth } });
     setData(result.data.photos);
+    setCurrentSearch(input);
+  };
+
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1);
+    console.log(currentSearch);
+
+    if (currentSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${
+        page + 1
+      }`;
+    }
+
+    let result = await axios.get(newURL, { headers: { Authorization: auth } });
+    setData(data.concat(result.data.photos));
   };
 
   useEffect(() => {
@@ -24,15 +44,19 @@ const HomePage = () => {
     <div style={{ minHeight: "100vh" }}>
       <Search
         search={() => {
-          search(searchURL);
+          let url = input === "" ? initURL : searchURL;
+          search(url);
         }}
         setInput={setInput}
       />
       <div className="pictures">
         {data &&
-          data.map((d) => {
-            return <Picture data={d} />;
+          data.map((d, index) => {
+            return <Picture key={index} data={d} />;
           })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morePicture}>更多圖片</button>
       </div>
     </div>
   );

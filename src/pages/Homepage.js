2c4dfed3eb24b26c2ec7193c34e03b2ef1978ea9
Allen Feng <haha30932@gmail.com>
2023-08-08 13:41:13 +0800

    init

---
+++ src/pages/Homepage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const HomePage = () => {
+  const auth = "BINLWIYb62j0NNqH5pvoWJSYbhwfbq6JXC6yDzkqQmsXCoKYLPU0KqDr";
+  return <div>Home Page</div>;
+};
+
+export default HomePage;
